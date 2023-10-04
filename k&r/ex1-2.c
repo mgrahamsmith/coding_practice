29c82c38b47f77b2e6e9a0c25a7092f0369fa096
Graham Smith <mgrahamsmith@protonmail.com>
2023-10-04 02:28:57 -0500

    Update ex1-2.c

--- k&r/ex1-2.c
+++ k&r/ex1-2.c
@@ -10,7 +10,7 @@ main()
 
    lower = 0;     /* lower limit of temperature table */
    upper = 300;   /* upper limit */
-   step = 20;      /* step size */
+   step = 20;     /* step size */
 
    fahr = lower;
    while (fahr <= upper) {
