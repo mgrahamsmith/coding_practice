fe52995f66e4c066d93c59fbf49351b9911b6190
Graham Smith <mgrahamsmith@protonmail.com>
2023-10-05 02:02:13 -0500

    Ex 1-1 through 1-5.

--- k&r/ex1-2.c
+++ k&r/ex1-2.c
@@ -1,21 +1,26 @@
-#include <stdio.h>
+/* Exercise 1-2.  Experiment to find out what happens when printf's argument
+string contains \c, where c is some character not listed above. */
 
-/* print Fahrenheit-Celcius table
-   for fahr = 0, 20, ..., 300 */
+#include <stdio.h>
 
 main()
 {
-   int fahr, celsius;
-   int lower, upper, step;
-
-   lower = 0;     /* lower limit of temperature table */
-   upper = 300;   /* upper limit */
-   step = 20;     /* step size */
-
-   fahr = lower;
-   while (fahr <= upper) {
-      celsius = 5 * (fahr-32) / 9;
-      printf("%d\t%d\n", fahr, celsius);
-      fahr = fahr + step;
-   }
+    printf("hello, world\y");
 }
+
+/* Output:
+>cl ex1-2.c
+Microsoft (R) C/C++ Optimizing Compiler Version 19.37.32822 for x86
+Copyright (C) Microsoft Corporation.  All rights reserved.
+
+ex1-2.c
+ex1-2.c(5): warning C4129: 'y': unrecognized character escape sequence
+Microsoft (R) Incremental Linker Version 14.37.32822.0
+Copyright (C) Microsoft Corporation.  All rights reserved.
+
+/out:ex1-2.exe
+ex1-2.obj
+
+>ex1-2.exe
+hello, worldy
+*/
---
+++ k&r/sec1-4.c
@@ -0,0 +1,42 @@
+/* Section 1.4  Symbolic Constants:
+... It's bad practice to bury "magic numbers" like 300 and 20 in a program;
+they convey little information to someone who might have to read the program later,
+and they are hard to change in a systematic way.  One way to deal with magic
+numbers is to give them meaningful names.  A #define line defines a *symbolic name*
+or *symbolic constant* to be a particular string of characters. */
+
+#include <stdio.h>
+
+#define  LOWER 0     /* lower limit of table */
+#define  UPPER 300   /* upper limit */
+#define  STEP  20    /* step size */
+
+/* print Fahrenheit-Celcius table */
+main()
+{
+    int fahr;
+
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+}
+
+/* Output:
+>ex1-5.exe
+  0  -17.8
+ 20   -6.7
+ 40    4.4
+ 60   15.6
+ 80   26.7
+100   37.8
+120   48.9
+140   60.0
+160   71.1
+180   82.2
+200   93.3
+220  104.4
+240  115.6
+260  126.7
+280  137.8
+300  148.9
+
+*/
