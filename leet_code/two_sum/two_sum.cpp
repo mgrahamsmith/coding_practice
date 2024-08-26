bea3899de9671784a6f70ccd260e9736f3b02383
Graham Smith <mgrahamsmith@protonmail.com>
2024-08-26 00:07:03 -0500

    Two Sum problem solution, O^2 time.

--- leet_code/two_sum/two_sum.cpp
+++ leet_code/two_sum/two_sum.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 #include <vector>
 
-int print_array(std::vector<int>& nums);
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target)
+    {
+        size_t length = nums.size();
+        int firstVal = 0;
+        int secondVal = 0;
+        for (size_t i { 0 }; i < length; ++i )
+        {
+            for (size_t j { 0 }; j < length; ++j)
+            {
+                if (nums[i] + nums[j] == target && i != j)
+                {
+                    firstVal = static_cast<int>(i);
+                    secondVal = static_cast<int>(j);
+                    break;
+                }
+            }
+        }
 
-int main()
-{
-    std::cout << "Two Sum Program\n---------------\n";
-
-    std::vector<int> nums {2,7,11,1,24};
-    std::cout << "nums:\n" << print_array(nums);
-    
-    return 0;
-}
+        return std::vector<int> { secondVal, firstVal };
+    }
+};
 
 // Print array contents
 int print_array(std::vector<int>& nums)
@@ -24,5 +36,42 @@ int print_array(std::vector<int>& nums)
     };
     std::cout << "]\n";
 
+    return 0;
+}
+
+
+int main()
+{
+    Solution solution {};
+
+    std::cout << "Two Sum Program\n---------------\n";
+    std::vector<int> nums;
+    int target;
+    std::vector<int> result(2);
+
+    std::cout << "Example 1\n";
+    std::cout << "Input: num = [2,7,11,15] , target = 9\n";
+
+    nums = { 2, 7, 11, 15 };
+    target = 9;
+    result = solution.twoSum(nums, target);
+    print_array(result);
+
+    std::cout << "Example 2\n";
+    std::cout << "Input: nums = [3,2,4], target = 6\n";
+
+    nums = { 3, 2, 4 };
+    target = 6;
+    result = solution.twoSum(nums, target);
+    print_array(result);
+
+    std::cout << "Example 3:\n";
+    std::cout << "Input: nums = [3,3], target = 6\n";
+
+    nums = { 3, 3 };
+    target = 6;
+    result = solution.twoSum(nums, target);
+    print_array(result);
+    
     return 0;
 }
