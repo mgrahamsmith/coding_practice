ae0eb389ea2597376a8df44d5aeaaff83375906d
Graham Smith <mgrahamsmith@protonmail.com>
2024-08-25 23:11:43 -0500

    Format edits.

--- learncpp/16_dynamic_arrays/01-03_vector_initialization.cpp
+++ learncpp/16_dynamic_arrays/01-03_vector_initialization.cpp
@@ -6,17 +6,15 @@ int print_array(std::vector<double>& nums);
 
 int main()
 {
-    std::cout << "Question 1:  Define a std::vector using CTAD and initialize"
-        << "with the first 5 positive square numbers (1, 4, 9, 16, and 25).\n";
+    std::cout << "Question 1:  Define a std::vector using CTAD and initialize with the first 5 positive square numbers"
+        << " (1, 4, 9, 16, and 25).\n";
 
     std::vector squares { 1, 4, 9, 15, 25 };
     std::cout << "nums:\n";
     print_array(squares);
 
-    std::cout << "Question #3: Define a std::vector (using an explicit"
-        << " template type argument) to hold the high temperature (to the"
-        << " nearest tenth of a degree) for each day of a year (assume 365"
-        << " days in a year).\n";
+    std::cout << "Question #3: Define a std::vector (using an explicit template type argument) to hold the high"
+        << " temperature (to the nearest tenth of a degree) for each day of a year (assume 365 days in a year).\n";
 
     std::vector<double> temps(365);
     std::cout << "temps:\n";
--- learncpp/16_dynamic_arrays/04_sum_and_prod.cpp
+++ learncpp/16_dynamic_arrays/04_sum_and_prod.cpp
@@ -3,13 +3,12 @@
 
 int main()
 {
-    std::cout << "Question #4: Using a std::vector, write a program that asks"
-        << " the user to enter 3 integral values. Print the sum and product"
-        << " of those values.\n\n";
+    std::cout << "Question #4: Using a std::vector, write a program that asks the user to enter 3 integral values."
+        << " Print the sum and product of those values.\n\n";
 
     std::vector<int> in_vals(3);
-    std::cout << "Enter 3 integers: ";
 
+    std::cout << "Enter 3 integers: ";
     std::cin >> in_vals[0] >> in_vals[1] >> in_vals[2];
 
     std::cout << "The sum is: " << in_vals[0] + in_vals[1] + in_vals[2] << '\n';
