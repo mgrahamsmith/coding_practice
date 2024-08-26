a9760cf86b6da8110ce89e6bcffb4eb6507ab375
Graham Smith <mgrahamsmith@protonmail.com>
2024-08-25 22:37:43 -0500

    Rename folders, add vector example.

--- learncpp/16_dynamic_arrays/vectors.cpp
+++ learncpp/16_dynamic_arrays/vectors.cpp
@@ -5,10 +5,12 @@ int print_array(std::vector<int>& nums);
 
 int main()
 {
-    std::cout << "Two Sum Program\n---------------\n";
+    std::cout << "Question 1:  Define a std::vector using CTAD and initialize"
+        << "with the first 5 positive square numbers (1, 4, 9, 16, and 25).\n";
 
-    std::vector<int> nums {2,7,11,1,24};
-    std::cout << "nums:\n" << print_array(nums);
+    std::vector squares { 1, 4, 9, 15, 25 };
+    std::cout << "nums:\n";
+    print_array(squares);
     
     return 0;
 }
@@ -17,12 +19,12 @@ int main()
 int print_array(std::vector<int>& nums)
 {
     std::cout << "size = " << nums.size() << "\n";
-    std::cout << "[" << nums[0]; // Print first element
+    std::cout << "{ " << nums[0];
     for (long unsigned int i = 1; i < nums.size(); i++)
     {
         std::cout << ", " << nums[i];
     };
-    std::cout << "]\n";
+    std::cout << " }\n";
 
     return 0;
 }
